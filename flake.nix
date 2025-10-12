5f04fd36605aba36a00fe69cf7ddc751ba56205f
Wyatt <johnson.wyatt.n@gmail.com>
2025-10-12 01:46:31 -0700

    changes!

--- flake.nix
+++ flake.nix
@@ -20,37 +20,34 @@
           inherit system overlays;
         };
       in {
-        packages = {
-          default = pkgs.rustPlatform.buildRustPackage {
-            pname = "huginn";
-            version = "0.1.0";
-            src = ./.;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
-            nativeBuildInputs = with pkgs; [
-              pkg-config
-            ];
-            buildInputs = with pkgs; [
-              librsvg
-              libsixel
-            ];
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "huginn";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
           };
-          huginn = self.packages.${system}.default;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          buildInputs = with pkgs; [
+            librsvg
+            libsixel
+          ];
         };
-        apps = {
-          default = {
-            type = "app";
-            program = "${self.packages.${system}.default}/bin/huginn";
-          };
-          huginn = self.apps.${system}.default;
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/huginn";
         };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             (rust-bin.stable.latest.default.override {
               extensions = ["rust-src" "rust-analyzer"];
             })
             rustfmt
+            pkg-config
             imagemagick
             librsvg
             libsixel
@@ -60,8 +57,11 @@
             mkdir -p $HOME/.local/share/huginn/logos
 
             if [ -d "${self}/logos" ]; then
-               cp -r ${self}/logos/* $HOME/.local/share/huginn/logos/
+               echo "Installing distro logos..."
+               cp -r ${self}/logos/* $HOME/.local/share/huginn/logos/ 2>/dev/null || true
             fi
+
+            echo "Huginn dev environment ready!"
           '';
         };
       }
